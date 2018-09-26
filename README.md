eaceed30454694913f153bd8d416caa95f763294
lberger1993 <berger.luciaeve@gmail.com>
2018-09-26 12:18:59 +0200

    Adds comments to watch the game

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ Mentioned also are the potential downside of their strategy. * None of these str
 #### 1. Simple Bettor - Early and Aggressive:
 This bettor bets on the strength of their hand and the order of bids.
 At the early stages of the game, they bet aggressively  with a lower threshold  `card > [50]` . As the rounds progress, they up the threshold and bet less aggressively after.
-This player may consistently overbid.
+In the long-run (lots of rounds),  this player may consistently overbid.
 
 #### 2. Distribution Bettor - Passive:
 This bettor bets more passively. If their card falls in the highest distribution of 100 with a shifting distribution per size, they will play it. Otherwise, they will not. In effect, they are more concerned with the overall score.
@@ -21,9 +21,9 @@ For example,
 ` n=3  y = card_max - (card_max/round_*4) `
 `y = 66 `
 At every round, this player bets with the probability of `p=0.25`. This better looks only at the independent probability and aims for the highest probability.
-At the beginning, this player will at most gain/lost 1 during the rounds. They will bid on a larger distribution during the second portion of the game.
+At the beginning, this player will at most gain/lost 1 during the rounds. During the second half, they will bid on a larger distribution.
 
 
 #### 3. Pattern Bettor
-This bettor examines the patterns of the other betters and builds their betting strategy off the original bets and wins of the other players.
-This bettor takes the highest previous score and the count from the round before. Using that information, they bets on that number (or percentage in the future). After enough rounds, they could build their model on the successful probability estimates of their fellow teammates betting strategies (Bayesian).
+This bettor examines the patterns of the other betters and builds their betting strategy off the history of wins of the other players.
+This bettor takes the highest previous score and the count from the round before. Using that information, they bets on that number (or percentage in the future). After enough rounds `[N=very large]`, they could build their model on the successful probability estimates of their fellow teammates betting strategies and perhaps build a Bayesian model.
--- wizard.py
+++ wizard.py
@@ -1,38 +1,53 @@
 import numpy as np
 
 
-def set_up_players(players_names):
+def set_up_players():
+    """
+    initializes the dictionary of players
+    """
     players = list()
     for player in PLAYERS_NAMES:
         players.append({"name": player, "cards": [], "bet": {"cards_bet": [], "card_history": []},
-                        "score": 0, "prev_score": 0, "count_played": 0, "count_of_cards_bet":0})
+                        "score": 0, "prev_score": 0, "count_played": 0,
+                        "change_in_score": 0, "player_strategy": ""
+                        })
     return players
 
 
-def run_rounds(compiled_players, number_of_rounds):
+def deal_cards(round_count, players):
+    """
+    uses np module to select random set of hands by N
+    :return: the selected cards & the updated player dictionary
+    """
     selected_cards = list()
+    for contestant in players:
+        selected_hand = np.random.random_integers(low=0, high=CARD_MAX, size=round_count)
+        contestant['cards'] = selected_hand
+        # compiles all the cards
+        selected_cards.extend(selected_hand)
+    return selected_cards
+
+
+def run_rounds(compiled_players, number_of_rounds):
+    """
+    counts, deals the cards, sets the strategies and sums the points
+    """
     for round_ in range(1, number_of_rounds + 1):
         print("** ROUND {0} **".format(round_))
-
-        # distributes the cards from 1 - 100 with the size round
-        for contestant in compiled_players:
-            selected_hand = np.random.random_integers(low=0, high=CARD_MAX, size=round_)
-            contestant['cards'] = selected_hand
-            selected_cards.extend(selected_hand)
+        chosen_cards = deal_cards(round_, compiled_players)
 
         # players select their betting strategy
         bet_early_aggressive(all_players[0], round_)
         bet_on_distribution(all_players[1], round_)
         bet_early_aggressive(all_players[2], round_)
-        bet_conditional_logic(all_players[3], round_)
-
-        selected_cards = sorted(selected_cards)
-        winning_cards = (selected_cards[len(selected_cards) - round_:len(selected_cards)])
+        bet_historical_pattern(all_players[3])
 
         # the winning cards are determined
+        selected_cards = sorted(chosen_cards)
+        winning_cards = (selected_cards[len(selected_cards) - round_:len(selected_cards)])
         print("___WINING CARD(S) {0}___".format(winning_cards))
-        # the point are scored
         sum_points(all_players, winning_cards)
+
         # the round is reset to the current round
         for participant in all_players:
             print("___Participant {0}___ Bets: {1} ___ Score: {2}"
@@ -40,35 +55,47 @@ def run_rounds(compiled_players, number_of_rounds):
 
             participant['cards'] = []
             participant['bet']['cards_bet'] = []
-        selected_cards = []
     winner = max(all_players, key=lambda x: x['score'])
-    print("**** WINNER {0} ****".format(winner['name']))
+    print("**** WINNER {0} & STRATEGY {1} ****".format(winner['name'], winner['player_strategy']))
 
 
 def bet_early_aggressive(player, round_):
+    """
+    player bets aggressively [ any card > 50] half the game
+    player reduces midway [ any card <75]
+    """
     for card in player['cards']:
-        if card > 50 and round_ <= 5:
+        if card > 50 and round_ <= ROUND_COUNT/2:
             player['bet']['cards_bet'].append(card)
-        if round_ > 5 and card > 75:
+        if round_ > ROUND_COUNT/2 and card > 75:
             player['bet']['cards_bet'].append(card)
+    player["player_strategy"] = "early_aggressive"
     return player
 
 
 def bet_on_distribution(player, round_):
+    """
+    bets on distribution of the cards and where they fall
+    bets less aggressively at the beginning and more at end
+    """
     for card in player['cards']:
-        if card > (CARD_MAX - (CARD_MAX/round_)) and round_ <= 5 :
+        if card > (CARD_MAX - (CARD_MAX/round_)) and round_ <= ROUND_COUNT/2:
             player['bet']['cards_bet'].append(card)
-        if card > (CARD_MAX - (round_ * 5)) and round_ > 5:
+        if card > (CARD_MAX - (round_ * 5)) and round_ > ROUND_COUNT/2:
             player['bet']['cards_bet'].append(card)
+    player["player_strategy"] = "distribution_late"
     return player
 
 
-def bet_conditional_logic(player, round_):
-    # Looks at the past winner => highest change in score with score positive
-    max_played = max(all_players, key=lambda x: x['count_of_cards_bet'])
+def bet_historical_pattern(player):
+    """
+
+    """
+    max_played = max(all_players, key=lambda x: x['change_in_score'])
     betting_top = np.argpartition(player['cards'], -max_played['count_played'])[-max_played['count_played']:]
     for card in betting_top:
         player['bet']['cards_bet'].append(card)
+    player["player_strategy"] = "historical_winners"
     return player
 
 
@@ -80,24 +107,24 @@ def sum_points(betting_members, card_winners):
         participant['count_played'] = len(bets)
         cards = participant['cards']
 
-        containing_cards = len(set(card_winners).intersection(set(cards)))
-        count_of_bets = len(bets)
+        count_actual_winners = len(set(card_winners).intersection(set(cards)))
+        count_predicted_winners = len(bets)
 
         # if they guess the exact contents
-        if count_of_bets == containing_cards and bets:
+        if count_predicted_winners == count_actual_winners and bets:
             participant['score'] = participant['score'] + len(bets) + 2
 
-        if count_of_bets > containing_cards:
-            participant['score'] = participant['score'] - abs(containing_cards - count_of_bets)
+        if count_predicted_winners > count_actual_winners:
+            participant['score'] = participant['score'] - abs(count_actual_winners - count_predicted_winners)
 
-        if count_of_bets < containing_cards:
-            participant['score'] = participant['score'] - abs(containing_cards - count_of_bets)
+        if count_predicted_winners < count_actual_winners:
+            participant['score'] = participant['score'] - abs(count_actual_winners - count_predicted_winners)
 
         # if bet 0 and have 0
-        if containing_cards == 0 and not bets:
+        if count_actual_winners == 0 and not bets:
             participant['score'] = participant['score'] + 2
 
-        participant['count_of_cards_bet'] = participant['score'] - participant['prev_score']
+        participant['change_in_score'] = participant['score'] - participant['prev_score']
 
     return betting_members
 
@@ -106,7 +133,7 @@ NUMBER_OF_CARDS = 100
 CARD_MAX = 100
 PLAYERS_NAMES = ["Angela", "Theresa", "Emmanuel", "Donald"]
 ROUND_COUNT = 10
-all_players = set_up_players(PLAYERS_NAMES)
+all_players = set_up_players()
 run_rounds(all_players, ROUND_COUNT)
 
 
